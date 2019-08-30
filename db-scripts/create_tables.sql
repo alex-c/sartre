7196de87284aa655780b074f304ee9088dc20703
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-08-30 17:21:15 +0200

    Added tables to SQL scripts

--- db-scripts/create_tables.sql
+++ db-scripts/create_tables.sql
@@ -1,5 +1,7 @@
 -- Creates the tables needed for Sartre.
 
+-- 1/3 users and roles
+
 CREATE TABLE users (
     login varchar (32) PRIMARY KEY,
     name varchar (255) NOT NULL UNIQUE,
@@ -14,9 +16,42 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE user_roles (
-    user_login varchar (32) REFERENCES users (login),
-    role_id integer REFERENCES roles (id)
+    user_login varchar (32) REFERENCES users (login) NOT NULL,
+    role_id integer REFERENCES roles (id) NOT NULL
 );
 
 INSERT INTO roles (name) VALUES ('admin');
 INSERT INTO roles (name) VALUES ('author');
+
+-- 2/3 blogs and posts
+
+CREATE TABLE blogs (
+    id varchar (32) PRIMARY KEY,
+    title varchar (255) NOT NULL UNIQUE,
+    description varchar (2048) NOT NULL
+);
+
+CREATE TABLE posts (
+    id varchar (32) PRIMARY KEY,
+    title varchar (255) NOT NULL UNIQUE,
+    published boolean DEFAULT false,
+    blog_id varchar (32) NOT NULL,
+    content text NOT NULL
+);
+
+CREATE TABLE blog_contributors (
+    blod_id varchar (32) REFERENCES blogs (id) NOT NULL,
+    user_login varchar (32) REFERENCES users (login) NOT NULL
+);
+
+CREATE TABLE post_authors (
+    post_id varchar (32) REFERENCES posts (id) NOT NULL,
+    user_login varchar (32) REFERENCES users (login) NOT NULL
+);
+
+-- 3/3 Sartre config
+
+CREATE TABLE sartre_config (
+    default_blog_id varchar (32) REFERENCES blogs (id)
+);
+
