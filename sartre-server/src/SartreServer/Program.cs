5332d80bc5ca1c52527a5a9017e8124b56dbd310
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-08-30 12:49:15 +0200

    Set up ASP.NET Core

--- sartre-server/src/SartreServer/Program.cs
+++ sartre-server/src/SartreServer/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace SartreServer
 {
@@ -6,7 +9,22 @@ namespace SartreServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TODO");
+            // Build and run web host
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    // Get/set environment info
+                    IHostingEnvironment environment = hostingContext.HostingEnvironment;
+                    config.SetBasePath(Directory.GetCurrentDirectory());
+
+                    // Add appsettings.json
+                    config.AddJsonFile("appsettings.json", optional: false)
+                          .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true);
+                });
     }
 }
---
+++ sartre-server/src/SartreServer/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SartreServer
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+
+        private ILoggerFactory LoggerFactory { get; }
+
+        public Startup(ILoggerFactory loggerFactory, IConfiguration configuration)
+        {
+            LoggerFactory = loggerFactory;
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
