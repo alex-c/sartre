d0e876990ac4fdfd1b9a84f6c39d5aa00e65419f
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-08-30 15:33:32 +0200

    Implemented `GetUsers` and `GetUser` in user service

--- sartre-server/src/SartreServer/Repositories/SqlRepositories/SqlUserRepository.cs
+++ sartre-server/src/SartreServer/Repositories/SqlRepositories/SqlUserRepository.cs
@@ -1,19 +1,45 @@
-﻿using SartreServer.Models;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using SartreServer.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 
 namespace SartreServer.Repositories.SqlRepositories
 {
     public class SqlUserRepository : IUserRepository
     {
+        private string ConnectionString { get; }
+
+        public SqlUserRepository(IConfiguration configuration)
+        {
+            ConnectionString = configuration.GetValue<string>("Database:ConnectionString");
+        }
+
+        internal IDbConnection GetNewConnection()
+        {
+            return new NpgsqlConnection(ConnectionString);
+        }
+
         public IEnumerable<User> GetAllUsers()
         {
-            throw new NotImplementedException();
+            IEnumerable<User> users = null;
+            using (IDbConnection connection = GetNewConnection())
+            {
+                users = connection.Query<User>("SELECT * FROM users");
+            }
+            return users;
         }
 
         public User GetUser(string login)
         {
-            throw new NotImplementedException();
+            User user = null;
+            using (IDbConnection connection = GetNewConnection())
+            {
+                user = connection.QueryFirstOrDefault<User>("SELECT * FROM users WHERE login=@Login", new { Login = login });
+            }
+            return user;
         }
 
         public void CreateUser(User user)
