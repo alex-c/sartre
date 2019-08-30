bc075bcb4c097fa4305626669b03eb52e6af947f
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-08-30 15:31:54 +0200

    Added a password field to the user model

--- sartre-server/src/SartreServer/Models/User.cs
+++ sartre-server/src/SartreServer/Models/User.cs
@@ -18,6 +18,11 @@ namespace SartreServer.Models
         public string Name { get; set; }
 
         /// <summary>
+        /// The user's hashed password.
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
         /// A short user biography.
         /// </summary>
         public string Biography { get; set; }
