bb5a39a579f85717bdeaf926c36adf37f139fbef
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-09-06 13:24:51 +0200

    Cleanup

--- sartre-ui/src/views/Home.vue
+++ sartre-ui/src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Post></Post>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Post from '@/components/Post.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Post,
   },
 })
 export default class Home extends Vue {}
