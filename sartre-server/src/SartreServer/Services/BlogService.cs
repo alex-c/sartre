5c4d27bd6c2af8cca6604b14a39dc2819b0efcd9
Alexandre Charoy <alexandre.charoy@gmail.com>
2020-02-04 13:29:05 +0100

    Added comments to the blog service

--- sartre-server/src/SartreServer/Services/BlogService.cs
+++ sartre-server/src/SartreServer/Services/BlogService.cs
@@ -7,14 +7,32 @@ using System.Collections.Generic;
 
 namespace SartreServer.Services
 {
+    /// <summary>
+    /// Provides access to blog data and blog management.
+    /// </summary>
     public class BlogService
     {
+        /// <summary>
+        /// A logger for local logging needs.
+        /// </summary>
         private ILogger Logger { get; }
 
+        /// <summary>
+        /// Provides access to blog data.
+        /// </summary>
         private IBlogRepository BlogRepository { get; }
 
+        /// <summary>
+        /// Provides access to blog post data.
+        /// </summary>
         private IPostRepository PostRepository { get; }
 
+        /// <summary>
+        /// Sets up the blog service.
+        /// </summary>
+        /// <param name="loggerFactroy">A factory to create loggers from.</param>
+        /// <param name="blogRepository">Access to blog data.</param>
+        /// <param name="postRepository">Access to blog post data.</param>
         public BlogService(ILoggerFactory loggerFactroy, IBlogRepository blogRepository, IPostRepository postRepository)
         {
             Logger = loggerFactroy.CreateLogger<UserService>();
@@ -22,11 +40,21 @@ namespace SartreServer.Services
             PostRepository = postRepository;
         }
 
+        /// <summary>
+        /// Gets all available blogs.
+        /// </summary>
+        /// <returns>Returns a list of blogs.</returns>
         public IEnumerable<Blog> GetAllBlogs()
         {
             return BlogRepository.GetAllBlogs();
         }
 
+        /// <summary>
+        /// Gets a blog by it's ID.
+        /// </summary>
+        /// <param name="blogId">ID of the blog to get.</param>
+        /// <returns>Returns the blog.</returns>
+        /// <exception cref="BlogNotFoundException">Thrown if there is no blog with the provided ID.</exception>
         public Blog GetBlog(string blogId)
         {
             Blog blog = BlogRepository.GetBlog(blogId);
@@ -40,11 +68,23 @@ namespace SartreServer.Services
             }
         }
 
+        /// <summary>
+        /// Gets a paginated portion of the posts of a blog.
+        /// </summary>
+        /// <param name="blogId">ID of the blog to get posts for.</param>
+        /// <param name="page">Page of posts.</param>
+        /// <param name="itemsPerPage">Items per page of posts.</param>
+        /// <returns>Returns the paginated blog posts.</returns>
         public IEnumerable<Post> GetBlogPosts(string blogId, int page, int itemsPerPage)
         {
             return PostRepository.GetPostsOfBlog(blogId, page, itemsPerPage);
         }
 
+        /// <summary>
+        /// Gets the contributors of a blog.
+        /// </summary>
+        /// <param name="blogId">ID of the blog to get the contributors of.</param>
+        /// <returns>Returns a list of users.</returns>
         public IEnumerable<User> GetBlogContributors(string blogId)
         {
             return BlogRepository.GetBlog(blogId).Contributors;
